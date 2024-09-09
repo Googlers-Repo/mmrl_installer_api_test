fdbe8db98753b223c29b04a0589d0610cb8f8dd8
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-09-09 18:02:52 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,17 +1,17 @@
-if [ "$MMRL_INTR" = "true" ]; then
-    mmrl_exec() { ui_print "#!mmrl:$*"; }
-    gui_print() { mmrl_exec color "\"$@\""; }
-    mmrl_setLastLine() { mmrl_exec setLastLine "\"$@\""; }
-    gui_image() { mmrl_exec addImage "$*"; }
-else
-    mmrl_exec() { true; }
-    gui_print() { ui_print "$@" | sed 's/<[A-Z.]*>//g'; }
-    mmrl_setLastLine() { true; }
-    gui_image() { true; }
-fi
+mmrl_exec() {
+    if [ "$MMRL_INTR" = "true" ]; then
+    local command=$1
+    shift
+    local args=$(printf "|%s" "$@")
+    args=${args:1}
+    echo "#!mmrl:<$command=($args)>"
+    fi
+}
 
-gui_print "<FG.MAGENTA>MMRL<R> <FG.BLUE>Install<R> <BG.RED>V2 API Test<R>"
+mmrl_exec color "<FG.MAGENTA>MMRL<R> <FG.BLUE>Install<R> <BG.RED>V3 API Test<R>"
 
-gui_image "$MODPATH/cover.png"
+echo "$MODPATH"
+
+mmrl_exec addImage "/$MODPATH/cover.png"
 
 abort "Not installed"
